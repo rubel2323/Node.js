82d5e8c9313a370a7084b8a00fdcc0064685b964
Rubel <mr.adcadre41@gmail.com>
2026-05-31 13:08:33 +0600

    delete method added by array.splice(index,number)

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -3,76 +3,142 @@ import { insertProduct, readProduct } from "../service/product.service";
 import type { Iproduct } from "../types/product.type";
 import { parseBody } from "../utility/parseBody";
 
-export const productController=async(req:IncomingMessage,res:ServerResponse)=>{
+export const productController = async (
+  req: IncomingMessage,
+  res: ServerResponse,
+) => {
+  const url = req.url; // '/', '/products',
+  const method = req.method;
 
-    
-const url=req.url;// '/', '/products',
-const method=req.method;
+  const urlparts = url?.split("/");
 
-const urlparts=url?.split('/');
+  const id =
+    urlparts && urlparts[1] === "products" ? Number(urlparts[2]) : null;
 
-const id=urlparts && urlparts[1]==='products'?Number(urlparts[2]):null;
+  if (url === "/products" && method === "GET") {
+    const products = readProduct();
+    res.writeHead(200, { "content-type": "application/json" });
+    res.end(
+      JSON.stringify({
+        message: "message retrieved successfully",
+        data: products,
+      }),
+    );
+  } else if (method === "GET" && id !== null) {
+    const products = readProduct();
+    const product = products.find((p: Iproduct) => p.id === id);
 
+    if (!product) {
+      res.writeHead(404, { "content-type": "application/json" });
+      res.end(
+        JSON.stringify({
+          message: "product not found",
+          data: null,
+        }),
+      );
+      return;
+    }
 
-
-
-if(url==='/products' && method==="GET"){
-const products=readProduct();
-   res.writeHead(200,{"content-type": "application/json"});
-    res.end(JSON.stringify(
-        {
-            message:"message retrieved successfully",
-            data: products,
-        }
-    )
-); 
-}
-
-else if(method ==="GET" && id !== null){
-    const products=readProduct();
-    const product=products.find((p : Iproduct)=>p.id === id);
-
-   res.writeHead(200,{"content-type": "application/json"});
-    res.end(JSON.stringify(
-        {
-            message:"message created successfully",
-            data: product,
-        }
-    )
-); 
-
-}
-
-else if(method === "POST" && url === "/products")
-    // created product  by Post Method
-{
- const products=readProduct();
-    const body= await parseBody(req);
+    res.writeHead(200, { "content-type": "application/json" });
+    res.end(
+      JSON.stringify({
+        message: "message created successfully",
+        data: product,
+      }),
+    );
+  } else if (
+    method === "POST" &&
+    url === "/products"
+  ) // created product  by Post Method
+  {
+    const products = readProduct();
+    const body = await parseBody(req);
     // console.log("Body",body)
-    const newProduct={
-        id: Date.now(),
-        ...body,
+    const newProduct = {
+      id: Date.now(),
+      ...body,
     };
 
     products.push(newProduct);
-    console.log(products);
-    insertProduct(products)
 
-      res.writeHead(200,{"content-type": "application/json"});
-    res.end(JSON.stringify(
-        {
-            message:"message created successfully",
-          
-        }
-    )
-); 
-}
+    insertProduct(products);
 
-// updated product by PUT method
-else if(method === "PUT" ){
-    
-}
+    res.writeHead(200, { "content-type": "application/json" });
+    res.end(
+      JSON.stringify({
+        message: "message created successfully",
+      }),
+    );
+  }
 
+  // updated product by PUT method
+  else if (method === "PUT" && id !== null) {
+    const body = await parseBody(req);
+    const products = readProduct();
 
+    const index = products.findIndex((p: Iproduct) => p.id === id);
 
-}
+    if (index < 0) {
+      res.writeHead(404, { "content-type": "application/json" });
+      res.end(
+        JSON.stringify({
+          message: "product not found",
+          data: null,
+        }),
+      );
+      return;
+    }
+
+    products[index] = {
+      id: products[index].id,
+      ...body,
+    };
+    insertProduct(products);
+
+    res.writeHead(200, {
+      "content-type": "application/json",
+    });
+
+    res.end(
+      JSON.stringify({
+        success: true,
+        message: "product updated successfully",
+        data: products[index],
+      }),
+    );
+  }
+
+  // remove products by DELETE method
+  else if (method === "DELETE" && id !== null) {
+    const products = readProduct();
+
+    const index = products.findIndex((p: Iproduct) => p.id === id);
+
+    if (index < 0) {
+      res.writeHead(404, { "content-type": "application/json" });
+      res.end(
+        JSON.stringify({
+          message: "product not found",
+          data: null,
+        }),
+      );
+      return;
+    }
+
+    products.splice(index, 1);
+    console.log("The deleted id has", id);
+    insertProduct(products);
+
+    res.writeHead(200, {
+      "content-type": "application/json",
+    });
+
+    res.end(
+      JSON.stringify({
+        success: true,
+        message: "product deleted successfully",
+        data: products[index],
+      }),
+    );
+  }
+};
--- src/service/product.service.ts
+++ src/service/product.service.ts
@@ -1,20 +1,17 @@
-
-import fs from "fs"
+import fs from "fs";
 import { readFileSync } from "fs";
 import path from "path";
 
-const filePath=path.join(process.cwd(),"./src/database/db.json");
+const filePath = path.join(process.cwd(), "./src/database/db.json");
 
-export const readProduct=()=>{
-  
-   const product= fs.readFileSync(filePath,"utf-8")
-   // console.log(product);
-   return JSON.parse(product);
-}
+export const readProduct = () => {
+  const product = fs.readFileSync(filePath, "utf-8");
+  // console.log(product);
+  return JSON.parse(product);
+};
 
 // write the file and send it  into db.json
-export const insertProduct=(payload:any)=>{
-   fs.writeFileSync(filePath,JSON.stringify(payload));
-   console.log(payload);
-
-}
+export const insertProduct = (payload: any) => {
+  fs.writeFileSync(filePath, JSON.stringify(payload));
+  // console.log(payload);
+};
